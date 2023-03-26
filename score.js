38836b92ae3caa2b2047891c44d8f1c890f81aab
sonohara <5350767+sonohara@users.noreply.github.com>
2023-03-26 23:20:34 +0900

    fix

--- score.js
+++ score.js
@@ -1,7 +1,7 @@
-const { webkit } = require('playwright');
+const { chromium } = require('playwright');
 
 (async () => {
-  const browser = await webkit.launch();
+  const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto('https://leetcode.com/sonohara/');
   await page.locator('div.shadow-level3', {has: page.locator('text=Solved Problems')}).screenshot({ path: `score.png` });
